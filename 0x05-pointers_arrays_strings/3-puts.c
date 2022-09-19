d17705acd901db50f56b3e24b28bcef0aaadd044
rexubuntu <ap.oyeniran@gmail.com>
2022-09-19 23:02:18 +0100

    0x05

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _puts - prints a string
+* @str: string to print
+* Description: prints a strin* On success: returns no error
+*/
+
+void _puts(char *str)
+
+{
+
+	int i = 0;
+
+		while (*(str + i) != '\0')
+
+	{
+
+			putchar(*(str + i));
+
+				i++;
+
+	}
+
+	putchar(10);
+
+}
